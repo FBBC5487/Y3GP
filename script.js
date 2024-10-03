33aa72b23eeb76c7a36fee3f6a020a21cff9234a
FBBC5487 <70328331+FBBC5487@users.noreply.github.com>
2024-10-04 00:28:14 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('changeMessageButton').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'You clicked the button! Welcome to the world of JavaScript.';
+});
