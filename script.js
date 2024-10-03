db3157b5e6bb7a7a59102ce29e7140e5570edc9a
FBBC5487 <70328331+FBBC5487@users.noreply.github.com>
2024-10-04 00:36:08 +0800

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('changeMessageButton').addEventListener('click', function() {
-    document.getElementById('message').textContent = 'You clicked the button! Welcome to the world of JavaScript.';
-});
